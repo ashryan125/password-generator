0d6ac8d931f823efc7a847fbb3817b6da4d67e3f
ashryan <20115553+ashryan125@users.noreply.github.com>
2021-05-06 20:09:44 -0400

    variables for random letters, numbers, and symbols

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,28 @@
 // Assignment code here
 
 
+//set of letters include lower case, upper case, numbers, special characters
+//generate lowercase letter
+function getLowerLetter() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+//generate uppercase letter
+function getUpperLetter() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+};
+//generate number letter
+function getNumber() {
+  return String.fromCharCode(Math.floor(Math.random() *10) + 48);
+};
+//generate symbol letter
+function getSymbol() {
+  var symbols = '!@#$%^&*(){}[]=<>,./';
+  return symbols[Math.floor(Math.random()* symbols.length)];
+};
+
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
